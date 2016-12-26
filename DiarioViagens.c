86aefb897b2320fcace0072842925c58396329d0
Smellypie <l_evora_d@hotmail.com>
2016-12-26 23:03:38 +0000

    Add files via upload

--- DiarioViagens.c
+++ DiarioViagens.c
@@ -10,28 +10,27 @@ struct Data
 struct Ano
 {
 	int ano;
-	struct Viagem *viagens;		//lista de viagens que ocorreram nesse ano
+	struct Viagem *viagens;												//lista de viagens que ocorreram nesse ano
 	struct Ano *seg;
 };
 
 struct Viagem
 {
-	struct Data diaIni;		//dia do início da viagem(dia mes)
-	int duracao;			//duração da viagem em dias
-	char *destinoP;			//destino da viagem(país)
-	char *destinoC;			//destino da viagem(cidade)
-	int meioT;			//meio de transporte(0 - carro, 1 - avião, ...)
-	int custo;			//custo total da viagem(euros)
-	int kmPercorridos;		//quilómetros percorridos durante a viagem
+	struct Data diaIni;													//dia do início da viagem(dia mes)
+	int duracao;														//duração da viagem em dias
+	char *destinoP;														//destino da viagem(país)
+	char *destinoC;														//destino da viagem(cidade)
+	int meioT;															//meio de transporte(0 - carro, 1 - avião, ...)
+	int custo;															//custo total da viagem(euros)
+	int kmPercorridos;													//quilómetros percorridos durante a viagem
 	struct Viagem *seg;
 };
 
 struct Ano *calendario;
 
-//adicionaViagem
 //leFicheiro
 
-void criaCalendario()
+void criaCalendario()													//Base da lista
 {
 	calendario = malloc(sizeof(struct Ano));
 	calendario -> ano = 0;
@@ -39,7 +38,7 @@ void criaCalendario()
 	calendario -> seg = NULL;
 }
 
-void adicionaAno(int ano)	//cria a struct de um novo ano
+void adicionaAno(int ano)												//Cria a struct de um novo ano (dá erro)
 {
 	struct Ano *aux, *aux2;
 	aux = malloc(sizeof(struct Ano));
@@ -54,30 +53,137 @@ void adicionaAno(int ano)	//cria a struct de um novo ano
 		if(calendario -> seg -> ano > ano)
 		{
 			aux -> ano = ano;
+			aux -> viagens = NULL;
 			aux -> seg = calendario -> seg;
 			calendario -> seg = aux;
 		}
 		else
 		{
 			aux = calendario -> seg;
-			while(ano > aux -> ano || aux -> seg != NULL)
+			while(aux -> ano < ano && aux != NULL)
 			{
 				aux2 = aux;
 				aux = aux -> seg;
 			}
-			if(aux -> seg == NULL)
+			if(aux == NULL)
 			{
 				aux -> ano = ano;
+				aux -> viagens = NULL;
 				aux -> seg = NULL;
-				aux = aux2;
 			}
 			else
 			{
 				aux -> ano = ano;
+				aux -> viagens = NULL;
+				aux -> seg = aux2 -> seg;
 				aux2 -> seg = aux;
 			}
 		}
 	}
+}
+
+struct Ano *procuraAno(int ano)											//verifica se determinado ano já está criado
+{
+	struct Ano *aux;
+	aux = calendario;
+	while(aux -> ano != ano && aux != NULL)
+	{
+		aux = aux -> seg;
+	}
+	if(aux == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		return aux;
+	}
+}
+
+int vemAntes(struct Data d1, struct Data d2)							//Compara as datas e devolve verdadeiro se a primeira data vem antes
+{
+	if(d1.mes < d2.mes)
+	{
+		return 1;
+	}
+	else
+	{
+		if(d1.mes > d2.mes)
+		{
+			return 0;
+		}
+		else //d1.mes == d2.mes
+		{
+			if(d1.dia < d2.dia)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+}
+
+void insereViagem(struct Ano *ano, struct Viagem *viagem)				//Insere viagem criada na lista de viagens ordenadas cronologicamente do respetivo ano
+{
+	if(ano -> viagens == NULL)
+	{
+		ano -> viagens = viagem;
+	}
+	else
+	{
+		//Por acabar
+	}
+}
+
+void adicionaViagem()
+{
+	int ano, dia, mes, duracao, meioT, custo, kmPercorridos;
+	char *destinoC, *destinoP;
+	struct Data auxD;
+	struct Ano *auxA;
+	struct Viagem *auxV;
+	auxV = malloc(sizeof(struct Viagem));
+	printf("Dia de inicio da viagem(dd mm aaaa): \n");
+	scanf("%d%d%d", &dia, &mes, &ano);
+	fflush(stdin);
+	printf("Destino da viagem\nPais: ");
+	scanf("%s", &destinoP);
+	fflush(stdin);
+	printf("Cidade: ");
+	scanf("%s", &destinoC);
+	fflush(stdin);
+	printf("Duracao da viagem(em dias): \n");
+	scanf("%d", &duracao);
+	fflush(stdin);
+	printf("Meio de transporte usado:\n1 - Aviao\n2 - Carro\n");
+	scanf("%d", &meioT);
+	fflush(stdin);
+	printf("Distancia percorrida durante a viagem(em quilometros): ");
+	scanf("%d", &kmPercorridos);
+	fflush(stdin);
+	printf("Custo total da viagem(em euros): ");
+	scanf("%d", &custo);
+	fflush(stdin);
+	
+	if(procuraAno(ano) == NULL)
+	{
+		adicionaAno(ano);
+	}
+	auxA = procuraAno(ano);
+	auxD.dia = dia;
+	auxD.mes = mes;
+	auxV -> diaIni = auxD;
+	auxV -> destinoC = destinoC;
+	auxV -> destinoP = destinoP;
+	auxV -> duracao = duracao;
+	auxV -> meioT = meioT;
+	auxV -> kmPercorridos = kmPercorridos;
+	auxV -> custo = custo;
+	auxV -> seg = NULL;
+	insereViagem(auxA, auxV);
 }
 
 void menu()
@@ -99,6 +205,12 @@ void menu()
 
 int main()
 {
+	criaCalendario();
+	//adicionaAno(2000);
+	adicionaAno(2001);
+	adicionaAno(2000);
+	adicionaViagem();
+	//printf("%d\n", calendario -> seg -> seg -> ano);
 	//leFicheiro();
 	//menu();
 	return 0;
